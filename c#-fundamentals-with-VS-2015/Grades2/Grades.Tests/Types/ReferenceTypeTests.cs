34dd639a8a2784785c70aef14fba6d1afa1e4997
Brad McKuhen <bmckuhen@yahoo.com>
2018-09-24 23:49:35 -0400

    additional module work
    This is everything up through Control FLow module

---
+++ c#-fundamentals-with-VS-2015/Grades2/Grades.Tests/Types/ReferenceTypeTests.cs
@@ -0,0 +1,98 @@
+﻿using Grades2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests.Types
+{
+    [TestClass]
+    public class ReferenceTypeTests
+    {
+
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+            AddGrades(grades);
+            Assert.AreEqual(89.1f, grades[1]);
+
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
+        public void ValueTypesPassByValue()
+        {
+            int x = 46;
+            IncrementNubmer(x);
+            Assert.AreEqual(x, 46);
+        }
+
+        private void IncrementNubmer(int number)
+        {
+            number += 1;
+        }
+
+        [TestMethod]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+            GiveBookAName(book2);
+            Assert.AreEqual(book1.Name, "A book");
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A book";
+            //DateTime.UtcNow.ToString();
+        }
+
+        [TestMethod]
+        public void StringComparison()
+        {
+            string name1 = "Scott";
+            string name2 = "scott";
+            bool result = String.Equals(name1, name2, System.StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void VariablesHoldAReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "g1name";
+            Assert.AreEqual(g2.Name, "g1name");
+        }
+
+        [TestMethod]
+        public void NamesAreEqual()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "g1name";
+            Assert.AreEqual(g2.Name, g1.Name);
+        }
+
+        [TestMethod]
+        public void IntVariablesHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+           
+        }
+
+    }
+}
--- c#-fundamentals-with-VS-2015/Grades2/Grades2/Program.cs
+++ c#-fundamentals-with-VS-2015/Grades2/Grades2/Program.cs
@@ -12,29 +12,71 @@ namespace Grades2
         static void Main(string[] args)
         {
 
-            //SpeechSynthesizer synth = new SpeechSynthesizer();
-            //synth.SelectVoice("");
-            //synth.Speak("Hello! This is the grade book program");
-
+            //*
             GradeBook book = new GradeBook();
+            book.NameChanged += OnNameChanged;
+            book.NameChanged += OnNameChanged2;
+            book.NameChanged += OnNameChanged2;
+            book.NameChanged -= OnNameChanged2;
+            //book.NameChanged += new NameChangedDelegate(OnNameChanged);
+            //book.NameChanged += new NameChangedDelegate(OnNameChanged2);
+            //book.NameChanged += new NameChangedDelegate(OnNameChanged2);
+            //book.NameChanged = null;
+
+            book.Name = "Scott's Grade Book";
+            book.Name = "Grade Book";
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
-/*            book.AddGrade(75);
-            book.AddGrade(75);
-            book.AddGrade(45);
-            book.AddGrade(35);
-            book.AddGrade(65);
-            book.AddGrade(55);
-            */
 
-            //GradeBook book2 = new GradeBook();
-            //book2.AddGrade(75);
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(book.Name);
+            WriteResult("Average: ", stats.AverageGrade);
+            WriteResult("Highest: ", (int)stats.HighestGrade);
+            WriteResult("Lowest: ", stats.LowestGrade);
+             //           */
+
+            /*
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "g1name";
+            Console.WriteLine(g2.Name);
+*/
+
             Console.ReadLine();
+
         }
+
+        static void OnNameChanged(string existingName, string newName)
+        {
+            Console.WriteLine($"Grade book changing name from {existingName} to {newName}");
+        }
+
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        }
+
+        static void OnNameChanged2(string existingName, string newName)
+        {
+            Console.WriteLine("***");
+        }
+
+        static void OnNameChanged2(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine("***");
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine($"{description}: {result:F2}");
+        }
+
     }
 }
