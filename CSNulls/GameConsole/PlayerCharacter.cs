b7ba2198e9f3a44f138557fce1b538b09838f018
Brad McKuhen <bmckuhen@yahoo.com>
2018-10-01 22:06:28 -0400

    about halfway through CS Nulls elimating null reference exceptions

---
+++ CSNulls/GameConsole/PlayerCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole
+{
+    class PlayerCharacter
+    {
+        public string Name { get; set; }
+        //public Nullable<int> DaysSinceLastLogin { get; set; }
+        //public Nullable<DateTime> DateOfBirth { get; set; }
+        public int? DaysSinceLastLogin { get; set;}
+        public DateTime? DateOfBirth{ get; set; }
+        public bool? IsNoob { get; set; }
+
+        public PlayerCharacter()
+        {
+            //            DateOfBirth = DateTime.MinValue; // magic number
+            //            DaysSinceLastLogin = -1; // magic number again
+            DateOfBirth = null;
+            DaysSinceLastLogin = null;
+        }
+    }
+}
---
+++ CSNulls/GameConsole/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var player = new PlayerCharacter();
+            //PlayerCharacter player = null;
+            //PlayerCharacter player = new PlayerCharacter();
+            //player.DaysSinceLastLogin = 42;
+
+            //int days;// = player.DaysSinceLastLogin ??  -1;
+            //int days = player?.DaysSinceLastLogin ?? -1;
+
+            //if (player!=null)
+            //{
+            //    days = player.DaysSinceLastLogin ?? -1;
+            //} else
+            //{
+            //    days = -1;
+            //}
+
+
+            //int days = player.DaysSinceLastLogin.Value;
+            //Console.WriteLine(days);
+
+            //player.Name = "Sarah";
+            //            player.DaysSinceLastLogin = 42;
+
+            //PlayerDisplayer.Write(player);
+            //player.DaysSinceLastLogin = 42;
+
+            //PlayerDisplayer.Write(player);
+
+            PlayerCharacter[] players = new PlayerCharacter[3]
+            {
+                new PlayerCharacter {Name="Sarah" },
+                new PlayerCharacter(), //name == null
+                null // PC == null
+            };
+
+            string p1 = players?[0]?.Name;
+            string p2 = players?[1]?.Name;
+            string p3 = players?[2]?.Name;
+
+            Console.ReadLine();
+        }
+    }
+}
